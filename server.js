c28b7ab9a0be8fa7ddd0de12633d42aa7c44b75a
Himanshu <himanshu3@myseneca.ca>
2022-03-22 01:23:19 -0400

    second

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
 
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', '.hbs');
 
 app.get("/employees", (req, res) => {
     data.getAllEmployees()
-    .then(data => res.render("employees", {employees: data }))
-    .catch(err => res.render("employees", {message: "no results"}))
+        .then(data => res.render("employees", { employees: data }))
+        .catch(err => res.render("employees", { message: "no results" }))
 });
 
 app.get("/employees/add", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/addEmployee.html"));    
+    res.sendFile(path.join(__dirname, "/views/addEmployee.html"));
 });
 
 app.post("/employees/add", (req, res) => {
     data.addEmployee(req.body)
-    .then(() =>  res.redirect("/employees"))    
+        .then(() => res.redirect("/employees"))
 });
 
-app.get("/PartTimer", (req,res) => {
-    data.getPartTimers().then((data)=>{
+app.get("/PartTimer", (req, res) => {
+    data.getPartTimers().then((data) => {
         res.json(data);
     });
 });
@@ -38,17 +38,15 @@ app.get("/description", (req, res) => {
     res.render("description");
 })
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send("Page Not Found");
 });
 
 
-data.initialize().then(function(){
-    app.listen(HTTP_PORT, function(){
+data.initialize().then(function() {
+    app.listen(HTTP_PORT, function() {
         console.log("app listening on: " + HTTP_PORT)
     });
-}).catch(function(err){
+}).catch(function(err) {
     console.log("unable to start server: " + err);
 });
-
-
